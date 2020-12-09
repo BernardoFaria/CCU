5766d226247583f6c777a7d08c95889aed60b20f
GoncaloFreitas <querido.freitas@gmail.com>
2020-12-09 22:10:32 +0000

    adicionar merdas ao user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,5 @@
+import 'package:trainsafe/session.dart';
+
 import '../report.dart';
 
 class User {
@@ -8,13 +10,32 @@ class User {
     Report(name: '*****', cc: '*****', lastSession: '12/09', covidTest: 'teste.png')
   ];
 
+  List<Session> activeSessions = [
+    Session(date: '12/12/20', begining: '11:00', end: '12:00')
+  ];
+
+  List<Session> expiredSessions = [
+    Session(date: '13/13/20', begining: '11:00', end: '12:00')
+  ];
+
+
+
 
   List<Report> get reportsList {
     return reports;
   }
 
-  void addReportsList(Report report) {
+  void addReport(Report report) {
     reports.add(report) ;
+  }
+
+  void addSession(Session session) {
+    activeSessions.add(session) ;
+  }
+
+  void activeSessionToExpiredSession(Session session) {
+    activeSessions.remove(session) ;
+    expiredSessions.add(session);
   }
 
   User({ this.uid });
